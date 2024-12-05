36619951148488da17d88e786a591fafd86c27d3
jonathanwirz <jonathan.wirz@icloud.com>
2024-12-05 14:29:08 +0100

    ;)

---
+++ BLJ-OOP/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Runtime.CompilerServices;
+using System.Windows;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        bool running = false;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        private void btnToggleRun_Click(object sender, RoutedEventArgs e)
+        {
+            if (running)
+            {
+                running = false;
+                tbStatus.Text = "Stopped";
+                btnToggleRun.Content = "Run";
+            }
+            else
+            {
+                running = true;
+                tbStatus.Text = "Running";
+                btnToggleRun.Content = "Stop";
+            }
+            
+        }
+    }
+}
